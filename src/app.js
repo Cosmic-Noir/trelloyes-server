14c26bb4df0f077dca25c6e5fa0dac87cfd9aa80
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-28 12:44:45 -0600

    Add error logging to API authorization and remove logged errors from git

--- src/app.js
+++ src/app.js
@@ -39,6 +39,18 @@ app.use(function errorHandler(error, req, res, next) {
   res.status(500).json(response);
 });
 
+// API validation:
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
 // Storage:
 const cards = [
   {
