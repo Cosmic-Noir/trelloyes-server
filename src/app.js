933b03556a68d338381fd402afe064046898b9e5
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-28 16:00:22 -0600

    Move list info to list-router.js. All checks for GET, POST, DELETE successful

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const uuid = require("uuid/v4");
 
 // Routing info:
 const cardRouter = require("./card/card-router");
+const listRouter = require("./list/list-router");
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use(function errorHandler(error, req, res, next) {
 
 // Call Routing info:
 app.use(cardRouter);
+app.use(listRouter);
 
 // API validation:
 app.use(function validateBearerToken(req, res, next) {
@@ -42,86 +44,6 @@ app.use(function validateBearerToken(req, res, next) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
   next();
-});
-
-// GET endpoints:
-
-app.get("/list", (req, res) => {
-  res.json(lists);
-});
-
-app.get("/list/:id", (req, res) => {
-  const { id } = req.params;
-  const list = list.find(li => li.id == id);
-
-  // Ensure list is found:
-  if (!list) {
-    logger.error(`List with id ${id} not found.`);
-    return res.status(404).send("List Not Found");
-  }
-});
-
-// POST endpoints:
-
-app.post("/list", (req, res) => {
-  const { header, cardIds = [] } = req.body;
-
-  if (!header) {
-    logger.error(`Header is required`);
-    return res.status(400).send("Invalid data");
-  }
-
-  // check card IDs
-  if (cardIds.length > 0) {
-    let valid = true;
-    cardIds.forEach(cid => {
-      const card = cards.find(c => c.id == cid);
-      if (!card) {
-        logger.error(`Card with id ${cid} not found in cards array.`);
-        valid = false;
-      }
-    });
-
-    if (!valid) {
-      return res.status(400).send("Invalid data");
-    }
-  }
-
-  // get an id
-  const id = uuid();
-
-  const list = {
-    id,
-    header,
-    cardIds
-  };
-
-  lists.push(list);
-
-  logger.info(`List with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/list/${id}`)
-    .json({ id });
-});
-
-// DELETE endpoints
-
-app.delete("/list/:id", (req, res) => {
-  const { id } = req.params;
-
-  const listIndex = lists.findIndex(li => li.id == id);
-
-  if (listIndex === -1) {
-    logger.error(`List with id ${id} not found.`);
-    return res.status(404).send("Not Found");
-  }
-
-  lists.splice(listIndex, 1);
-
-  logger.info(`List with id ${id} deleted.`);
-  res.status(204).end();
 });
 
 module.exports = app;
